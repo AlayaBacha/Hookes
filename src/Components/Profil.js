2e50a5fe139495e63589731b79a915c9dd67166d
Your Name <alayabacha@gmail.com>
2023-05-08 16:22:39 +0200

    router

--- src/Components/Profil.js
+++ src/Components/Profil.js
@@ -1,13 +1,21 @@
-import { useParams } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 
 const Profil =({movies})=>{
     const {id}=useParams()
     const found = movies.find (el => el.id == id)
+    const navigate=useNavigate()
     return(
         <div className="mara">
         <h1>{found.title}</h1>
+        <h3>Description :</h3>
         <h4>{found.discription}</h4>
         <iframe width="560" height="315" src={found.annonce} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        {/* <button className="nn"><Link to={"/"}>HOME</Link></button> */}
+        <div className="ll"> 
+        <button className="nn" onClick={()=>navigate('/')}>HOME</button>
+        <button className="kk" onClick={()=>navigate('/listmovie')}>Movies</button>
+        
+        </div>
         </div>
     )
 }
